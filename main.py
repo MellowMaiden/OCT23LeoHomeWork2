9f2c595333f2a27d19998c324d6904f607d08b85
MellowMaiden <l2628787@gmail.com>
2023-10-24 14:20:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello Stephen this is Leo's Homework")
+x=int(input("how many chocolate do you have")) # this is to tell how many chocolate do you have
+y=int(input("how many friend do you have")) # this is to tell how many friend do you have
+a=x//y # this is to calculate how many chocolate you need to send to your friend
+z=x%y # this is to calculate how many chocolate you left after share with your friend
+print("you can send your friend",a,"chocolate left",z,"chocolate") # this is final resualt
+quit() # this is the end step
